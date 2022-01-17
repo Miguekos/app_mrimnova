92fb30c5eea9caf26728cf3fb895e5686db18a45
Diego Urbina <diego.urbina@acceso.com.pe>
2022-01-16 19:54:16 -0500

    Se agregó nuevos módulos al sistema

---
+++ src/pages/Grupos.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="q-pa-sm">
+    <TitleGeneric
+      icon="face"
+      color="green"
+      @click="openModal"
+      titulo="Grupos"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GroupsComponent",
+  components: {
+    TitleGeneric: () => import("src/components/Title.vue"),
+  },
+  data() {
+    return {
+      isModalCreate: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalCreate = true;
+    }
+  },
+};
+</script>
+
+<style></style>
