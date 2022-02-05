7c8612201e43681b04908b47ec0bfadc0cb12395
Diego Urbina <diego.urbina@acceso.com.pe>
2022-02-04 21:17:26 -0500

    Se culminó con el desarrollo del módulo de Proveedores

---
+++ src/enums/shared.js
@@ -0,0 +1,10 @@
+export const typeDocuments = [
+    {
+      label: "DNI",
+      value: "0",
+    },
+    {
+      label: "RUC",
+      value: "1",
+    },
+  ];
---
+++ src/mixins/forms/supplierSchema.js
@@ -0,0 +1,16 @@
+const supplierSchema = {
+    data() {
+      return {
+        form: {
+          name: null,
+          address_main: null,
+          email: null,
+          type_document: "0",
+          document: null,
+        },
+      };
+    },
+  };
+  
+  export { supplierSchema };
+  
